e1cfdc86a0cb31d9e235df4310f5d0f21b91e4bd
Woojin J. Lim <hailcryptic@gmail.com>
2024-07-27 11:53:11 +0900

    🏗️ Dto change: modified update user dto

--- src/main/java/com/honeyosori/dogfile/domain/user/dto/UpdateUserDto.java
+++ src/main/java/com/honeyosori/dogfile/domain/user/dto/UpdateUserDto.java
@@ -5,9 +5,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.honeyosori.dogfile.global.constant.Role;
 import com.honeyosori.dogfile.global.constant.UserStatus;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Date;
 
 public record UpdateUserDto(@NotNull String password,
                             @NotNull String profileImageUrl,
+                            @DateTimeFormat(pattern="yyyy-MM-dd") @Past @NotNull Date birthday,
                             @NotNull String phoneNumber,
                             @NotNull String email,
                             @NotNull Role role,
