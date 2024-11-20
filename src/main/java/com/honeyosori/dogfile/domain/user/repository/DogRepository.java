6791e5d41ea79f229f4932bb8f493e1070aaecba
Monggu-Park <tomas407@naver.com>
2024-11-20 20:27:28 +0900

    🎨 Resolve error

--- src/main/java/com/honeyosori/dogfile/domain/user/repository/DogRepository.java
+++ src/main/java/com/honeyosori/dogfile/domain/user/repository/DogRepository.java
@@ -4,11 +4,12 @@ import com.honeyosori.dogfile.domain.user.dto.UserDogDto;
 import com.honeyosori.dogfile.domain.user.entity.Dog;
 import com.honeyosori.dogfile.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@Repository
 public interface DogRepository extends JpaRepository<Dog, String> {
     List<Dog> findAllByDogfileUserEmail(String dogfileUserEmail);
 }
