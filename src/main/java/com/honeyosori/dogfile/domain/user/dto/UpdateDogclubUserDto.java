452dc43983073f9c454d5b15daf371f80a5bc2ee
김상윤 <tkddbszhel@gmail.com>
2025-02-09 02:44:41 +0900

    🐛 Fix simple bug

--- src/main/java/com/honeyosori/dogfile/domain/user/dto/UpdateDogclubUserDto.java
+++ src/main/java/com/honeyosori/dogfile/domain/user/dto/UpdateDogclubUserDto.java
@@ -1,10 +1,8 @@
 package com.honeyosori.dogfile.domain.user.dto;
 
 import lombok.Builder;
-import lombok.Setter;
 
 @Builder
-@Setter
 public record UpdateDogclubUserDto(
         String accountName,
         String profileImageUrl
--- src/main/java/com/honeyosori/dogfile/domain/user/dto/UpdateDogusUserDto.java
+++ src/main/java/com/honeyosori/dogfile/domain/user/dto/UpdateDogusUserDto.java
@@ -1,11 +1,8 @@
 package com.honeyosori.dogfile.domain.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Setter;
 
 @Builder
-@Setter
 public record UpdateDogusUserDto(
         String accountName,
         String profileImageUrl
